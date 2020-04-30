c02c6625fe7d803772509ed882bec64fcf1e9759
minhvan251 <minhvan251@gmail.com>
2020-04-30 04:56:20 -0400

    add header template for all pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './shop/shop.component'
 import Header from './components/header/header.component'
+import SignInAndSignOutPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 
 
 function App() {
@@ -14,6 +15,8 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/signin' component={SignInAndSignOutPage} />
+
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './sign-in-and-sign-out.styles.scss';
+
+class SignInAndSignOutPage extends React.Component {
+    constructor() {
+        super();
+
+    }
+    render() {
+
+        return (
+            <div> </div>
+
+        )
+
+    }
+}
+export default SignInAndSignOutPage
